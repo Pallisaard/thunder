efb7592a895d92dbb2d02efff864b0bb508515bd
Rasmus Pallisgaard <rasm796h@gmail.com>
2024-11-08 19:55:22 +0100

    (doc) added documentation to AutoInitMeta class

--- thunder/utils/autoinitmeta.py
+++ thunder/utils/autoinitmeta.py
@@ -2,6 +2,28 @@ import abc
 
 
 class AutoInitMeta(abc.ABCMeta):
+    """
+    AutoInitMeta is a metaclass that automatically initializes instance attributes
+    based on class-level attributes and keyword arguments passed to the constructor.
+
+    When a class uses AutoInitMeta as its metaclass, the following behavior is added:
+    - All class-level attributes that are not callable and do not start with "__"
+        are set as instance attributes during initialization.
+    - All keyword arguments passed to the constructor are set as instance attributes.
+
+    Attributes:
+            mcs: The metaclass.
+            name: The name of the class being created.
+            bases: A tuple containing the base classes of the class being created.
+            namespace: A dictionary containing the class namespace.
+            kwargs: Additional keyword arguments.
+
+    Methods:
+            __new__(mcs, name, bases, namespace, **kwargs): Creates a new class with the
+            specified name, bases, and namespace, and modifies its __init__ method to
+            automatically set instance attributes.
+    """
+
     def __new__(mcs, name, bases, namespace, **kwargs):
         cls = super().__new__(mcs, name, bases, namespace, **kwargs)
         original_init = cls.__init__
